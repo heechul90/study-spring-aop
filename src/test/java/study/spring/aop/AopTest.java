55391e1445379cc8f26c9d74b25b47eede2138b2
hclee <heechul4296@gmail.com>
2022-03-26 19:13:03 +0900

    스프링 aop 구현1 - 시작

--- src/test/java/study/spring/aop/AopTest.java
+++ src/test/java/study/spring/aop/AopTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import study.spring.aop.order.OrderRepository;
 import study.spring.aop.order.OrderService;
+import study.spring.aop.order.aop.AspectV1;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class)
 public class AopTest {
 
     @Autowired
