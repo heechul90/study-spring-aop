bb6a37116d372ae8ffccfbd4e59cbd192f794834
hclee <heechul4296@gmail.com>
2022-03-27 20:33:25 +0900

    프록시 기술과 한계 - 스프링의 해결책

---
+++ src/test/java/study/spring/aop/proxyvs/ProxyDITest.java
@@ -0,0 +1,31 @@
+package study.spring.aop.proxyvs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import study.spring.aop.member.MemberService;
+import study.spring.aop.member.MemberServiceImpl;
+import study.spring.aop.proxyvs.code.ProxyDIAspect;
+
+@Slf4j
+@Import(ProxyDIAspect.class)
+//@SpringBootTest(properties = "spring.aop.proxy-target-class=false") //JDK 동적 프록시
+//@SpringBootTest(properties = "spring.aop.proxy-target-class=true") //CGLIB 프록시
+@SpringBootTest
+public class ProxyDITest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberServiceImpl memberServiceImpl;
+
+    @Test
+    void go() {
+        log.info("memberService class={}", memberService.getClass());
+        log.info("memberServiceImpl class={}", memberServiceImpl.getClass());
+        memberServiceImpl.hello("hello");
+    }
+}
